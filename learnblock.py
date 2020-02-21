0d77ab13fdd7279d6521e7e4767a4c8c85a77281
RebeGM19 <rebegarcia12@hotmail.com>
2020-02-21 13:12:59 +0100

    Import de módulos y funciones del proyecto LearnBlock relacionados con el parser de BlockText a Python

--- learnblock.py
+++ learnblock.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from learnbot_dsl.learnbotCode.Parser import __parserFromString, __generatePy, cleanCode
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,6 +12,7 @@ def get_blocks():
     if request.method == 'POST':
         blocks = request.get_json()
         blocksList = format_blocks(blocks)
+        prueba()
     return '', 200
 
 def format_blocks(blocks):
@@ -22,6 +24,60 @@ def format_blocks(blocks):
     for block in array:
         print(block)
     return array
+
+def prueba():
+    textprueba = """
+
+
+    when hay_alguien_triste = function.is_there_somebody_sad():
+    	if 1 < time_hay_alguien_triste:
+    		function.expressSadness()
+    	end
+    end
+
+    when start:
+    	function.look_up()
+    	function.expressNeutral()
+    end
+
+    when hay_alguien_sorprendido = function.is_there_somebody_surprised():
+    	if 1 < time_hay_alguien_sorprendido:
+    		function.expressSurprise()
+    	end
+    end
+
+    when hay_alguien_enfadado = function.is_there_somebody_angry():
+    	if 1 < time_hay_alguien_enfadado:
+    		function.expressAnger()
+    	end
+    end
+
+    when hay_alguien_neutral = function.is_there_somebody_neutral():
+    	if 1 < time_hay_alguien_neutral:
+    		function.expressNeutral()
+    	end
+    end
+
+    when hay_alguien_alegre = function.is_there_somebody_happy():
+    	if 1 < time_hay_alguien_alegre:
+    		function.expressJoy()
+    	end
+    end
+
+    """
+    try:
+        print(textprueba)
+        print("---------------Parser from string---------------\n\n")
+        print(__parserFromString(textprueba))
+        text = __generatePy(__parserFromString(textprueba))
+        print("--------------Generate py----------------\n\n")
+        print(text)
+        text = cleanCode(_code=text)
+        print("--------Clean code--------\n\n", text)
+    except ParseException as pe:
+        print(pe.line)
+        print(' ' * (pe.col - 1) + '^')
+        print(pe)
 
 
 if __name__ == '__main__':

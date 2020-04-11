86424d1ca3a05e049de2bf083f731bea152668de
RebeGM19 <rebegarcia12@hotmail.com>
2020-04-11 19:35:12 +0200

    Inicio conversor entre los distintos formatos Json de los bloques (LearnBlock a Blockly): Funciona con los bloques de control (hay que ajustar detalles)

---
+++ blocksLoader.py
@@ -0,0 +1,90 @@
+import json
+
+def loadBlocksPrueba(route):
+    f = open(route, "r")
+    if f.mode == 'r':
+        blocks = f.read()
+    result = json.loads(blocks)
+    print(len(result))
+    convertLB2Blockly(result)
+    return result
+
+def getParameters(variables):
+    resultParameters = ""
+    for par in variables:
+        if par["type"] == "float" or par["type"] == "int":
+            parType = "field_number"
+            parName = "NUM"
+            resultParameters += "{\"type\": \"field_number\", \"name\": \"NUM\", \"value\": 0}, "
+    resultParameters = resultParameters[:-2]
+    return resultParameters, len(variables)
+
+# Part of the shape is given in the name of the block
+def getNameAndShape(block, name, shape):
+    resultShape = ""
+    bkyName = name.upper()
+    if shape == "block8":  # Main blocks' shape
+        resultShape = "\"message0\": \"%{BKY_" + bkyName + "}\", \"message1\": \"%1\", \"args1\": [{\"type\": \"input_statement\", \"name\": \"DO\"}], "
+
+    if shape == "block6":  # If/Elif blocks' shape
+        resultShape = "\"message0\": \"%{BKY_" + bkyName + "} %1\", \"args0\": [{\"type\": \"input_value\", \"name\": \"IF\", \"check\": null}], \"message1\": \"%1\", \"args1\": [{\"type\": \"input_statement\", \"name\": \"DO\"}], \"previousStatement\": null, \"nextStatement\": null, "
+
+    if shape == "block5":  # Else/Forever blocks' shape
+        resultShape = "\"message0\": \"%{BKY_" + bkyName + "}\", \"message1\": \"%1\", \"args1\": [{\"type\": \"input_statement\", \"name\": \"DO\"}], \"previousStatement\": null, \"nextStatement\": null, "
+
+    if shape == "block4":  # ElapsedTime1 blocks' shape
+        resultShape = "\"message0\": \"%{BKY_" + bkyName + "}\", "
+        if block["variables"] != None:
+            resultShape += "\"args0\": [" + getParameters(block["variables"])[0] + "], "
+        resultShape += "\"output\": null, "
+
+    if shape == "block3":  # ElapsedTime2 blocks' shape
+        if block["variables"] != None:
+            inputPos = getParameters(block["variables"])[1]+1
+            resultShape = "\"message0\": \"%{BKY_" + bkyName + "} %" + str(inputPos) + "\", "
+            resultShape += "\"args0\": [" + getParameters(block["variables"])[0] + ", {\"type\": \"input_value\", \"name\": \"NUM\", \"check\": null}], "
+        else:
+            resultShape = "\"message0\": \"%{BKY_" + bkyName + "} %1\", "
+            resultShape += "\"args0\": [{\"type\": \"input_value\", \"name\": \"NUM\", \"check\": null}], "
+        resultShape += "\"output\": null, "
+
+    if shape == "block1":  # Wait blocks' shape
+        resultShape = "\"message0\": \"%{BKY_" + bkyName + "}\", "
+        if block["variables"] != None:
+            resultShape += "\"args0\": [" + getParameters(block["variables"])[0] + "], "
+        resultShape += "\"previousStatement\": null, \"nextStatement\": null, "
+
+    return resultShape
+
+def getColour(category):
+    colour = 0
+    if category == "control":
+        colour = 60
+    return colour
+
+def convertBlock(block, shape, alt):
+    vType = block["type"]
+    vName = block["name"]
+    vCategory = block["category"]
+    if vType == "others":
+        vCategory = "f" + vCategory
+    blocklyJson = "{\"type\": \"" + vCategory + "_" + vName.replace(" ", "_") + alt + "\", "
+    blocklyJson += "\"blocktextname\": \"" + vName + "\", "
+    blocklyJson += getNameAndShape(block, vName.replace(" ", "_"), shape)
+    blocklyJson += "\"colour\": 60"
+
+    return blocklyJson
+
+def convertLB2Blockly(lbJson):
+    blocklyJson = "["
+    for b in lbJson:
+        vShape = b["shape"]
+        for i in range (0, len(vShape)):
+            alt = "_" + str(i)
+            if len(vShape) == 1:
+                alt = ""
+            blocklyJson += convertBlock(b, vShape[i], alt)
+            blocklyJson += "}, "
+    blocklyJson = blocklyJson[:-2]
+    blocklyJson += "]"
+    print(blocklyJson)
--- learnblock.py
+++ learnblock.py
@@ -2,7 +2,15 @@ from flask import Flask, render_template, request
 from learnblockCode.guiCreateBlock import *
 from learnblockCode.Parser import __parserFromString, __generatePy, cleanCode, parserLearntBotCodeFromCode
 from btParser import processVars, parserBlockText
+from blocksLoader import loadBlocksPrueba
 app = Flask(__name__)
+
+# Loads a file
+def loadBlocks(route):
+    f = open(route, "r")
+    if f.mode == 'r':
+        result = f.read()
+    return result
 
 @app.route("/")
 def init():
@@ -19,15 +27,9 @@ def init():
     distances = loadBlocks("blocks/distances.json")
     ground = loadBlocks("blocks/ground.json")
 
+    loadBlocksPrueba("Control.conf")
+
     return render_template('index.html', control=control, operators=operators, values=values, variables=variables, emotions=emotions, speaker=speaker, base=base, motor=motor, camera=camera, distances=distances, ground=ground)
-
-
-# Loads a file
-def loadBlocks(route):
-    f = open(route, "r")
-    if f.mode == 'r':
-        result = f.read()
-    return result
 
 # Gets the variables declared by the user
 def getVars(result):
@@ -35,6 +37,24 @@ def getVars(result):
     variables = result[:finalVars]
     return variables
 
+# Mirar esto ?????
+def formatBlocks(blocks):
+    blocksList = blocks.split(">")
+    array = []
+    array.append("<?xml version=\"1.0\"?>")
+    for i in range(1, len(blocksList)-2):
+        blocksList[i] = blocksList[i] + ">"
+        array.append(blocksList[i])
+    for block in array:
+        print(block)
+    return array
+
+# Mirar esto ?????
+def listToString(s):
+    str1 = ""
+    for ele in s:
+        str1 += ele
+    return str1
 
 # When the button "execute" is clicked, gets the blocks in the workspace, parses them and returns the equivalent Block-Text (and Python) code
 @app.route("/result", methods=['GET', 'POST'])
@@ -57,25 +77,6 @@ def getBlocks():
         return result, 200
     else:
         return '', 200
-
-# Mirar esto ?????
-def formatBlocks(blocks):
-    blocksList = blocks.split(">")
-    array = []
-    array.append("<?xml version=\"1.0\"?>")
-    for i in range(1, len(blocksList)-2):
-        blocksList[i] = blocksList[i] + ">"
-        array.append(blocksList[i])
-    for block in array:
-        print(block)
-    return array
-
-# Mirar esto ?????
-def listToString(s):
-    str1 = ""
-    for ele in s:
-        str1 += ele
-    return str1
 
 
 if __name__ == '__main__':

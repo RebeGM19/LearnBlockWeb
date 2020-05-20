666fb048f5d49d8e71d0f351af678250bbbce791
RebeGM19 <rebegarcia12@hotmail.com>
2020-05-20 20:21:40 +0200

    Corrección del parser

--- parser.py
+++ parser.py
@@ -51,17 +51,18 @@ def insertBlock(firstBlock, block, specification):
 
 # Gets the block type, given by its id
 def getType(string):
-    positionFinal = string.find("_")
     finalType = None
-    type_ = string[:positionFinal]
-    if type_ == "control":
-        finalType = CONTROL
-    if type_ == "procedures":
-        finalType = USERFUNCTION
-    if type_ == "variables":
-        finalType = VARIABLE
-    if type_ == "base" or type_ == "camera" or type_ == "distances" or type_ == "emotion" or type_ == "ground" or type_ == "motor" or type_ == "speaker" or type_ == "fcontrol":
-        finalType = FUNTION
+    if string != None:
+        positionFinal = string.find("_")
+        type_ = string[:positionFinal]
+        if type_ == "control":
+            finalType = CONTROL
+        if type_ == "procedures":
+            finalType = USERFUNCTION
+        if type_ == "variables":
+            finalType = VARIABLE
+        if type_ == "base" or type_ == "camera" or type_ == "distances" or type_ == "emotion" or type_ == "ground" or type_ == "motor" or type_ == "speaker" or type_ == "fcontrol":
+            finalType = FUNTION
     return finalType
 
 # Returns the variables created by the user
@@ -75,29 +76,30 @@ def processVariables(variablesBlock, listBlocks):
 # Otherwise, if the block is a variable, returns that variable
 def getVariables(blockTree):
     result = None
-    # Blocks with "variables" on their type are variables
-    if "variables" in blockTree.get('type'):
-        blockTree.set('blocktextname', blockTree.find('field').text)
-        if blockTree.get('type') == "variables_set_dynamic":  # Variable's setter
+    if blockTree.get('blocktextname') != None:
+        # Blocks with "variables" on their type are variables
+        if "variables" in blockTree.get('type'):
+            blockTree.set('blocktextname', blockTree.find('field').text)
+            if blockTree.get('type') == "variables_set_dynamic":  # Variable's setter
+                if blockTree.find('field') != -1:
+                    if result == None:
+                        result = []
+                    # Only the first value at the right of the setter will be assigned
+                    rightBlock = blockTree.find('value').find('block')
+                    value = rightBlock.find('field').text
+                    if "text" in rightBlock.get('type'):
+                        value = "\"" + rightBlock.find('field').text + "\""
+                    result.append(value)
+        # Are not variables, but parameters
+        elif blockTree != None:
             if blockTree.find('field') != -1:
-                if result == None:
-                    result = []
-                # Only the first value at the right of the setter will be assigned
-                rightBlock = blockTree.find('value').find('block')
-                value = rightBlock.find('field').text
-                if "text" in rightBlock.get('type'):
-                    value = "\"" + rightBlock.find('field').text + "\""
-                result.append(value)
-    # Are not variables, but parameters
-    elif blockTree != None:
-        if blockTree.find('field') != -1:
-            for field in blockTree.findall('field'):
-                if result == None:
-                    result = []
-                param = field.text
-                if field.get('name') == "TEXT":
-                    param = "\"" + field.text + "\""
-                result.append(param)
+                for field in blockTree.findall('field'):
+                    if result == None:
+                        result = []
+                    param = field.text
+                    if field.get('name') == "TEXT":
+                        param = "\"" + field.text + "\""
+                    result.append(param)
     return result
 
 
@@ -150,7 +152,8 @@ def getStatements(statTree, firstBlock):
     # If the first block has other blocks inside it, processes those blocks (BottomIn, getStatements)
     if statement != None:
         # Recursion to get the elements inside the block
-        result = getStatements(statement, createBlock(statement.get('blocktextname'), getType(statement.get('type')), getVariables(statement), statement))
+        insertBlock(firstBlock, createBlock(statement.find('block').get('blocktextname'), getType(statement.find('block').get('type')), getVariables(statement.find('block')), statement.find('block')), "BOTTOMIN")
+        result = getStatements(statement, firstBlock)
 
     # If the first block has other blocks under it, processes those blocks (Bottom, getNext)
     if next_ != None:
